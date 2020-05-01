8e32f06b67cf6719f56431c66c393afdf5b6fc02
Vlad-0213 <61988430+Vlad-0213@users.noreply.github.com>
2020-05-01 16:26:37 -0600

    done
    Holden did it!!!

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,22 @@
+document.write("\"Trout baseball's best player,"
++ "he has won three MVP\'s in his first 7 years in the majors.\""
++ "-only Albert Pujols has done it before");
+
+var B = "Three1count" + "baseball updates"
+document.write(B)
+
+var Team = "The Yankees", Manager = "Boone" , Captain = "Judge",
+    Catcher = "Sanchez" , Pitcher = "Cole"
+document.write(Manager);
+
+var blues = "This text is blue"
+blues= blues.fontcolor("red");
+document.write(blues)   
+
+document.write(10+ 5);
+
+function My_First_Function() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text") .innerHTML = result;
+}
