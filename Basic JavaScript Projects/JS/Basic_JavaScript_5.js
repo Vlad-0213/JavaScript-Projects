a3c5b7a83ff942a4db99de0ca6ff51bed6d77a89
Vlad-0213 <61988430+Vlad-0213@users.noreply.github.com>
2020-05-04 15:07:41 -0600

    done

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,83 @@
+function my_Function() {
+    document.getElementById("Test") .innerHTML = 0/0;
+}
+
+
+function my_Function() {
+    document.getElementById("Test") .innerHTML = isNaN('This is a String');
+}
+
+function my_Function() {
+    document.write(3E459)
+}
+
+
+function my_Function() {
+    document.write(-5E898)
+}
+
+function my_Function() {
+    document.write(15< 5);
+}
+
+function my_Function() {
+    document.write(22 > 8)
+}
+
+function my_Function() {
+    console.log(5 + 4 )
+}
+
+function my_Function() {
+    document.write("15" + 8)
+}
+
+function my_Function() {
+    document.write(15 == 15)
+}
+
+function my_Function() {
+    document.write(5 == 17)
+}
+
+function my_Function() {
+    X = 20;
+    Y = 20
+    document.write(X === Y)
+}
+
+function my_Function() {
+    X = 85;
+    Y = "85";
+    document.write(X === Y)
+}
+
+function my_Function() {
+    V = "Team Sport";
+    z = "Team Sport";
+    document.write(V === Z)
+}
+
+function my_Function() {
+    document.write(10 > 7 && 15 > 2)
+}
+
+function my_Function() {
+    document.write(7 > 10 && 15 > 2)
+}
+
+function my_Function() {
+    document.write(7 > 10 || 15 > 2)
+}
+
+function my_Function() {
+    document.write(7 > 10 || 15 > 30)
+}
+
+function not_Function() {
+    document.getElementById("Not") .innerHTML = !(40 > 10)
+}
+
+function not_Function() {
+    document.getElementById("Not") .innerHTML = !(10 > 40)
+}
