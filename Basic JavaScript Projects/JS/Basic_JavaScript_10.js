6875e7d968046940440c1c387265c786043e9e44
Vlad-0213 <61988430+Vlad-0213@users.noreply.github.com>
2020-06-15 11:45:14 -0600

    fixed
    Fixed

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_10.js
@@ -0,0 +1,73 @@
+function Count_To_Ten() {
+    var Digit ="";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Counting").innerHTML = Digit;
+}
+
+function myFunction() {
+    var str = "Hello World!";
+    var n = str.length;
+    document.getElementById("length").innerHTML = n;
+  }   
+  
+var Instruments = ["Ball", "Bat", "Glove", "Bases", "Uniform"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function constant_Function() {
+    const Baseball_Gear = {type:"Glove", brand:"Wilson", color:"Brown"};
+    Baseball_Gear.color = "black";
+    Baseball_Gear.price = "$250";
+    document.getElementById("Constant").innerHTML = "The cost of the " +
+        Baseball_Gear.type + " was " + Baseball_Gear.price;
+    
+    }
+
+    var X = 90;
+    document.write(X);
+    {
+        let X = 45;
+        document.write("<br>" + X);
+    }
+    document.write("<br>" + X);
+
+    var X = 90;
+    document.write(X);
+    {
+        var X = 45;
+        document.write("<br>" + X);
+    }
+    document.write("<br>" + X);
+
+    let baseball = {
+        team: "Dodgers",
+        league: "National league",
+        year: "2020",
+        color: "Blue",
+        description: function() {
+            return "The car is a " + this.year + this.color + this.make + this.model;
+            }
+    };
+    document.getElementById("baseball_Object").innerHTML = baseball_description();
+
+    function break_Function() {
+        var text = "";
+        var i;
+        for (i = 5; i < 8; i++) {
+            if (i === 8) {
+                break;
+            }
+            text += "The number is " + i + "<br>";
+           }
+           document.getElementById("break").innerHTML = text;
+    }
--- Basic JavaScript Projects/JS/Basic_JavaScript_9.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_9.js
@@ -3,7 +3,7 @@ function countdown() {
 
     function tick() {
         seconds = seconds - 1;
-        Timer.innerHTML = seconds;
+        timer.innerHTML = seconds;
         setTimeout(tick, 1000);
     if(seconds == -1) {
         alert("Time's up!")
